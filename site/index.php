968bf5fb93e637bb41391322ec3405c1b141e027
ezawa <erocktoruchang@gmail.com>
2014-03-29 07:17:52 +0900

    defalt files

---
+++ site/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<title>レイアウト練習</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.11.0/build/cssreset/cssreset-min.css">
+		<link rel="stylesheet" type="text/css" href="/site_lesson/site/test.css">
+	</head>
+		<body id="load">
+			<div id="container">
+			<div id="header">
+				<h1><div id="mainPicture"></div></h1>
+			</div>
+			<div id="right">></div>
+			<div id="left"><</div>
+			<div id="thumbArea">
+			</div>
+			<div id="menu">
+				<ul>
+					<li><a href="">TOPICS</a></li>
+					<li><a href="">MEDIA</a></li>
+					<li><a href="">LIVE</a></li>
+					<li><a href="">DISCOGRAPHY</a></li>
+					<li><a href="">BIOGRAPHY</a></li>
+				</ul>
+			</div><!-- "#menu" -->
+			
+			<div id="main">
+			
+				<div id="contents">
+					<h2>TOPICS</h2>
+						<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+						<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+					<ul class="products">
+						<li>
+							<img src="/site_lesson/picture/a7x.jpeg" width="80" height="80">
+							<p>説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。</p>
+						</li>
+						<li>
+							<img src="/site_lesson/picture/a7x.jpeg" width="80" height="80">
+							<p>説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。</p>
+						</li>
+						<li>
+							<img src="/site_lesson/picture/a7x.jpeg" width="80" height="80">
+							<p>説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。</p>
+						</li>
+					</ul><!-- ".products" -->
+					<h2>見出し</h2>
+						<div class="sample">
+							<span class="badge">new!</span>
+							<img src="/site_lesson/picture/a7x.jpeg" width="60" height="60">
+							<p>補足！</p>
+						</div>
+						<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+						<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+					
+				</div><!-- "#contents" -->
+			
+				<div id="sidebar">
+					<h3>見出し</h3>
+					<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+					<h3>見出し</h3>
+					<p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+				</div><!-- "#sidebar" -->
+			
+			</div><!-- "#main" -->	
+			<div id="footer">2013-08-27 A7x "http://csscite/cite.php"</div>
+			
+			</div><!-- "#container" -->
+			<script src="/site_lesson/jquery_file/jquery-1.10.2.js"></script>
+				<script type="text/javascript" src="/site_lesson/site/slideShow.js"></script>
+		</body>
+</html>
